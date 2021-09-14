48658db3ee05ff55888227451610c7fa78465cd7
Ryan Leckey <ryan@launchbadge.com>
2021-09-13 21:31:03 -0700

    implement scanAccounts and sync using the mirror and node APIs

--- src/families/hedera/js-scanAccounts.ts
+++ src/families/hedera/js-scanAccounts.ts
@@ -1,18 +1,29 @@
 import { CurrencyNotSupported } from "@ledgerhq/errors";
 import { Observable } from "rxjs";
 import {
+  Account,
   CryptoCurrency,
   DerivationMode,
   DeviceId,
   ScanAccountEvent,
   SyncConfig,
 } from "../../types";
+import { open, close } from "../../hw";
+import {
+  getDerivationModesForCurrency,
+  runDerivationScheme,
+} from "../../derivation";
+import getAddress from "../../hw/getAddress";
+import BigNumber from "bignumber.js";
+import {
+  getAccountPlaceholderName,
+  emptyHistoryCache,
+} from "../../account";
+import { getAccountsForPublicKey } from "./api/mirror";
 
 export default function scanAccounts({
   currency,
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   deviceId,
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   scheme,
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   syncConfig,
@@ -25,7 +36,65 @@ export default function scanAccounts({
   syncConfig: SyncConfig;
   preferredNewAccountScheme?: DerivationMode;
 }): Observable<ScanAccountEvent> {
-  throw new CurrencyNotSupported("hedera currency not supported", {
-    currencyName: currency.name,
+  var derivationMode = scheme ?? getDerivationModesForCurrency(currency)[0];
+
+  return Observable.create((o) => {
+    void async function() {
+      let transport;
+
+      try {
+        let index = 0;
+        let addressPath = runDerivationScheme(derivationMode, currency, { account: index });
+        
+        transport = await open(deviceId);
+
+        // NOTE: asking for the address in hedera will return only the public key
+        let res = await getAddress(transport, {
+          currency,
+          path: addressPath,
+          derivationMode,
+        });
+
+        // use a mirror node to ask for any accounts that have
+        // this public key registered
+        let accounts = await getAccountsForPublicKey(res.publicKey);
+
+        for (let account of accounts) {
+          o.next({type: "discovered", account: {
+            type: "Account",
+            id: `js:2:${currency.id}:${account.accountId}:${derivationMode}`,
+            seedIdentifier: account.accountId,
+            derivationMode,
+            index,
+            freshAddress: "", // deprecated
+            freshAddressPath: "", // deprecated
+            freshAddresses: [], // deprecated
+            name: `${currency.name} ${account.accountId}`,
+            starred: false,
+            used: true,
+            currency,
+            balance: account.balance ?? new BigNumber(0),
+            spendableBalance: account.balance ?? new BigNumber(0),
+            operationsCount: 0,
+            operations: [],
+            swapHistory: [],
+            pendingOperations: [],
+            unit: currency.units[0],
+            creationDate: new Date(),
+            lastSyncDate: new Date(),
+            blockHeight: 0,
+            balanceHistoryCache: emptyHistoryCache,
+          } as Account});
+        }
+
+        o.complete();
+      } catch (err) {
+        o.error(err);
+      } finally {
+        if (transport != null) {
+          close(transport, deviceId);
+        }
+      }
+    }();
   });
 }
--- src/families/hedera/js-sync.ts
+++ src/families/hedera/js-sync.ts
@@ -1,13 +1,29 @@
 import { CurrencyNotSupported } from "@ledgerhq/errors";
 import { Observable } from "rxjs";
 import { Account, SyncConfig } from "../../types";
+import { getAccountBalance } from "./api/network";
 
 export default function sync(
   initialAccount: Account,
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   syncConfig: SyncConfig
 ): Observable<(account: Account) => Account> {
-  throw new CurrencyNotSupported("hedera currency not supported", {
-    currencyName: initialAccount.currency.name,
+  return Observable.create((o) => {
+    void async function() {
+      try {
+        const accountBalance = await getAccountBalance(initialAccount.seedIdentifier);
+
+        o.next((acc: Account) => ({
+          ...acc,
+
+          balance: accountBalance.balance,
+          spendableBalance: accountBalance.balance,
+        } as Account));
+
+        o.complete();
+      } catch (err) {
+        o.error(err);
+      }
+    }();
   });
 }
